c15b266919628c02d9873ea4879944ea5196cc2f
Jonatas Santos <jonatas.santos@produsti.com.br>
2022-12-22 11:30:06 -0300

    ✨ Add http methods for the teacher controller

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using smartschool_webapi.Data;
+using smartschool_webapi.Models;
 
 namespace smartschool_webapi.Controllers
 {
@@ -35,12 +36,93 @@ namespace smartschool_webapi.Controllers
             {
                 var result = await Repository.GetTeacherAsyncById(teacherId, true);
 
-                return Ok(result);
+                return Ok(result != null ? result : "Professor não encontrado!");
             }
             catch (Exception e)
             {
                 return BadRequest($"Erro: {e.Message}");
             }
+        }
+
+        [HttpGet("ByStudent/{studentId}")]
+        public async Task<IActionResult> getByDisciplineId(int studentId)
+        {
+            try
+            {
+                var result = await Repository.GetTeachersAsyncByStudentId(studentId, true);
+
+                return Ok(result != null ? result : "Professor não encontrado!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"Erro: {e.Message}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> post(Teacher teacher)
+        {
+            try
+            {
+                Repository.Add(teacher);
+
+                if (await Repository.SaveChangesAsync())
+                {
+                    return Ok(teacher);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut("{teacherId}")]
+        public async Task<IActionResult> put(int teacherId, Teacher teacher)
+        {
+            try
+            {
+                var teacherFounded = await Repository.GetTeacherAsyncById(teacherId, false);
+                if(teacherFounded == null) return NotFound("Professor não encontrado!");
+
+                Repository.Update(teacher);
+
+                if(await Repository.SaveChangesAsync())
+                {
+                    return Ok(teacher);
+                }                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete("{teacherId}")]
+        public async Task<IActionResult> delete(int teacherId)
+        {
+            try
+            {
+                var teacherFounded = await Repository.GetTeacherAsyncById(teacherId, false);
+                if(teacherFounded == null) return NotFound("Professor não encontrado!");
+
+                Repository.Delete(teacherFounded);
+
+                if(await Repository.SaveChangesAsync())
+                {
+                    return Ok("Teacher deletado!");
+                }                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Erro: {ex.Message}");
+            }
+
+            return BadRequest();
         }
     }
 }
